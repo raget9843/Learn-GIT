6be9f3a22e328793bb4b9096fec8db3e96e2a7d4
TEGAR RABBANI <TEGAR.RABBANI@FILESSI.LOCAL>
2025-08-10 16:07:56 +0700

    add divide feature

--- function.js
+++ function.js
@@ -5,4 +5,7 @@ const subtract = (a, b) => a - b;
 console.log(subtract(5,2));
 
 const multiply = (a, b) => a * b;
-console.log(multiply)
+console.log(multiply(2*2));
+
+const divide = (a, b) => a / b;
+console.log(divide(6,2));
